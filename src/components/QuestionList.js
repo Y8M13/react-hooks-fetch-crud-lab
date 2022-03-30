f854d5b8539dfc507a5bc0c06e5849b27657bbd9
Yolanda Morris <y.mijoe7145@gmail.com>
2022-03-29 22:13:03 -0400

    Initial Commit

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function QuestionList() {
+
+const QuestionList = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((response) => response.json())
+    .then((data) => {
+      setQuestions(data.question);
+    });
+  }, []);
   return (
+    //console.log(data)
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        <li>{setQuestions.map((question) => question.prompt)}</li>
+      </ul>
     </section>
+    
   );
-}
+
 
 export default QuestionList;
